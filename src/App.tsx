eb12c59e10b1f7c5b65f2459f84baaa8d6c5733a
Rafael <vampirorp13@gmail.com>
2024-12-23 00:22:51 -0300

    Firebase Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import About from './pages/about/about';
 import { AnimatePresence } from 'framer-motion';
 import { useLocation, useRoutes } from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
+import { initializeApp } from 'firebase/app';
 
 export default function App() {
   const element = useRoutes([
@@ -17,6 +18,22 @@ export default function App() {
       element: <About />,
     },
   ]);
+  // Import the functions you need from the SDKs you need
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDTpVx-XUizFRwwUYvxE8fpI5aghSBf11g',
+    authDomain: 'booreal-lab.firebaseapp.com',
+    projectId: 'booreal-lab',
+    storageBucket: 'booreal-lab.firebasestorage.app',
+    messagingSenderId: '224509341196',
+    appId: '1:224509341196:web:ea3a20c1ae5cd7c2938829',
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
   const location = useLocation();
 
